82cb06c0bb81c64377f28fcea07905d51caaebda
newtonneto <newton.mgs@gmail.com>
2022-01-25 10:52:05 -0300

    mudanca de mock

--- src/main/java/com/newton/services/DBService.java
+++ src/main/java/com/newton/services/DBService.java
@@ -113,7 +113,7 @@ public class DBService {
 		estado.saveAll(Arrays.asList(est1, est2));
 		cidade.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "newton.mgs@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "newton@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
