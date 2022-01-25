05dbf3cd2e8f10f25cded34f5692f94b46f63903
newtonneto <newton.mgs@gmail.com>
2022-01-25 13:26:39 -0300

    prod config class

---
+++ src/main/java/com/newton/config/ProdConfig.java
@@ -0,0 +1,41 @@
+package com.newton.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.newton.services.DBService;
+import com.newton.services.EmailService;
+import com.newton.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			//Caso deseje parar a recriação do DB sempre que o programa for reinicializado, setar o valor de,
+			//spring.jpa.hibernate.ddl-auto para none em applications-dev.properties
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
