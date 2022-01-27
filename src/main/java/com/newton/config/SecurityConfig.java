543ddbfbdc762ca1fa8d9b8ad205fef81a52b2a7
newtonneto <newton.mgs@gmail.com>
2022-01-27 13:40:53 -0300

    authorization

--- src/main/java/com/newton/config/SecurityConfig.java
+++ src/main/java/com/newton/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,10 +20,12 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.newton.security.JWTAuthenticationFilter;
+import com.newton.security.JWTAuthorizationFilter;
 import com.newton.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS = {
 		"/h2-console/**"
@@ -30,8 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 		"/produtos/**",
-		"/categorias/**",
-		"/clientes/**"
+		"/categorias/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+		"/clientes/**",
 	};
 	
 	@Autowired
@@ -51,10 +57,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_GET).permitAll() //Libera os requests POST dos caminhos informados
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() //Libera os requests GET dos caminhos informados
 			.antMatchers(PUBLIC_MATCHERS).permitAll() //Libera os requests dos caminhos informados
 			.anyRequest().authenticated(); //Exige autenticação do resto
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //Impede criação de sessão
 	}
 	
